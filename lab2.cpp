f30233f4262c9b597f03fca8518b75493462b126
edjo93 <eddas.carrasco@unitec.edu>
2019-07-08 16:45:39 -0600

    menu & main added

---
+++ lab2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+//prototypes
+
+
+int main(){
+	menu();
+	return 0;//el programa termina correctamente
+}
+
+//functions
+
+//menu con opciones
+void menu(){
+	
+	cout<<"\nmenu\n1.mostrar N numeros de una secuencia\n2.numeros defectivos\n3.piedra,papel o tigera"<<endl;
+}
