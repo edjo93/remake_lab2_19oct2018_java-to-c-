9d1a96ad8f51cbdb9fe740fe8fa974856702427b
edjo93 <eddas.carrasco@unitec.edu>
2019-07-08 23:07:27 -0600

    done 1rst option

--- lab2.cpp
+++ lab2.cpp
@@ -7,15 +7,51 @@ using std::cin;
 void menu();
 
 int main(){
+	int n;
 	int respuesta;
+	int counter;//control del ciclo
+	//valores iniciales de la sucesion
+	int var1;
+	int var2;
 	do{
 		menu();
 		cin>>respuesta;
 		//capturamos la opcion
 		switch(respuesta){
 			case 1:
+				//reiniciamos los valores
+				counter=1;
+				var1=0;
+				var2=4;
+				n=0;	
+				while(n<1){
+					cout<<"\ningrese n [n>0]: ";
+					cin>>n;
+				}
+				//n es un dato valido
+				//se despliega la sucesion
 
-				cout<<"\ncase1"<<endl;
+				while(counter<=n){
+					if(counter!=n){//se despliega la coma
+						if(counter%2==0){//variable en posicion par
+							cout<<var2<<",";
+							var2+=2;
+						}else{//variable en posicion impar
+							cout<<var1<<",";
+							var1+=2;
+						}
+					}else{
+						if(counter%2==0){//variable en posicion par
+							cout<<var2;
+							var2+=2;
+						}else{//variable en posicion impar
+							cout<<var1;
+							var1+=2;
+						}	
+					}
+					counter++;//el ciclo tiene que avanzar	
+				}
+				cout<<endl;			
 				break;
 			case 2:
 				cout<<"\ncase2"<<endl;
